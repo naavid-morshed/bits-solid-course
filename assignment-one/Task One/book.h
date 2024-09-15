5dd42a861f2b164c6a516e813b5f726e6a47ab13
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-09-15 10:03:46 +0600

    Task One Complete

---
+++ assignment-one/Task One/book.h
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#ifndef BOOK_H
+#define BOOK_H
+
+class Book
+{
+private:
+    std::string title;
+    std::string author;
+    std::string isbn;
+    bool isAvailable;
+
+public:
+    Book(std::string title, std::string author, std::string isbn, bool isAvailable) : title(title), author(author), isbn(isbn), isAvailable(isAvailable)
+    {
+        if (title.empty())
+            throw std::invalid_argument("Title cannot be empty");
+        if (author.empty())
+            throw std::invalid_argument("Author cannot be empty");
+        if (isbn.empty())
+            throw std::invalid_argument("ISBN cannot be empty");
+    }
+
+    void setTitle(std::string title)
+    {
+        this->title = title;
+    }
+
+    void setAuthor(std::string author)
+    {
+        this->author = author;
+    }
+
+    void setIsbn(std::string isbn)
+    {
+        this->isbn = isbn;
+    }
+
+    void setIsAvailable(bool isAvailable)
+    {
+        this->isAvailable = isAvailable;
+    }
+
+    bool getIsAvailable()
+    {
+        return this->isAvailable;
+    }
+
+    void displayBookDetails()
+    {
+        std::cout << "Title: " << this->title << "\n";
+        std::cout << "Author: " << this->author << "\n";
+        std::cout << "ISBN: " << this->isbn << "\n";
+        std::cout << "Availability: " << (this->isAvailable ? "Available" : "Unavailable\n") << std::endl;
+    }
+};
+#endif
---
+++ assignment-one/Task One/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "book.h"
+
+int main()
+{
+    Book book("Wuthering Heights", "Emily Bronte", "978-0-306-40615-7", true);
+    book.displayBookDetails();
+}
