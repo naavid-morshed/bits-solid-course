952eb69118e5c6059304705ccb2e2ae1224dd665
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-10-30 12:45:28 +0600

    fixed and completed task2

--- Session3_Assignment/Task_Two/main.cpp
+++ Session3_Assignment/Task_Two/main.cpp
@@ -4,15 +4,15 @@
 
 int main()
 {
-    Computer computer = ComputerBuilder()
-                            .setCpu("14900k")
-                            .setStorage("990 Pro Evo")
-                            .setGpu("409D")
-                            .setRam("Adata 7500mzh 32GB")
-                            .build();
+    ComputerBuilder computerBuilder = ComputerBuilder()
+                                          .setCpu("14900k")
+                                          .setRam("Adata 7500mzh 32GB");
 
-                            computer = ComputerBuilder()
-                            .setCpu("asdf")
+    computerBuilder = computerBuilder
+                          .setStorage("990 Pro Evo")
+                          .setGpu("409D");
+
+    Computer computer = computerBuilder
                             .build();
 
     ThirdPartyApiComputer cmp = ComputerAdapter::adaptToTPApiComputer(computer);
